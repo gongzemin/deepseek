cff44854d88fa0a203f0dbceb0173d8d25a8967b
gongzemin <andersonmingz@gmail.com>
2025-07-16 17:16:19 +0800

    Removes unused axios import
    Eliminates an unused axios import from PromptBox.tsx to improve code clarity and reduce unnecessary dependencies.

--- src/components/PromptBox.tsx
+++ src/components/PromptBox.tsx
@@ -9,7 +9,6 @@ import Image from 'next/image'
 import { assets } from '@/assets/assets'
 import { useAppContext } from '@/context/AppContext'
 import toast from 'react-hot-toast'
-import axios from 'axios'
 
 import { MessageType } from '@/types'
 
